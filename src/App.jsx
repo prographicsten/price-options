0eaefc4e99d455c494f5d4fbcfe4141456953bb4
prographicsten <prographicsten@gmail.com>
2023-09-18 21:04:23 +0600

    Line Chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import LineChart from './components/LineChart/LineChart';
 // import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
@@ -13,6 +14,7 @@ function App() {
       <NavBar></NavBar>
       {/* <DaisyNav></DaisyNav> */}
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   )
 }
---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,26 @@
+import { LineChart as LChart, Line, XAxis } from 'recharts';
+
+const LineChart = () => {
+
+    const studentMarks = [
+        { id: 1, studentName: "Rokeya", math: 85, chemistry: 78, physics: 92 },
+        { id: 2, studentName: "Fatema", math: 70, chemistry: 88, physics: 75 },
+        { id: 3, studentName: "Suraya", math: 95, chemistry: 86, physics: 91 },
+        { id: 4, studentName: "Khushi", math: 80, chemistry: 85, physics: 78 },
+        { id: 5, studentName: "Bou", math: 92, chemistry: 70, physics: 88 },
+    ];
+
+      
+    return (
+        <div className='px-4'>
+            <LChart width={350} height={400} data={studentMarks}>
+                <XAxis dataKey="studentName" />
+                <Line dataKey="math"></Line>
+                <Line dataKey="chemistry" stroke="green"></Line>
+                <Line dataKey="physics" stroke="red"></Line>
+            </LChart>
+        </div>
+    );
+};
+
+export default LineChart;
